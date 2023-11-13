a65519efa28eaf69b3f99a5fa0a4c65c54b6ecc2
sergei-zhirnov <s.zhirnov@protonmail.com>
2023-11-13 23:11:45 +0400

    M3T6, Try 1

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.task6;
+import java.lang.Math;
 
 public class FloatTypeCasting {
-    public void roundNumber(float numberToBeRounded) {
+    public static void roundNumber(float numberToBeRounded) {
+        int result = Math.round(numberToBeRounded);
+        System.out.println(result);
     }
+  /*  public static void main(String[] args) {
+        roundNumber(4.7f);
+    }
+   */
 }
